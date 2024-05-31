afdc84749ef03bbed48d4267134360022e3f8a22
yeftakun <yeftakun34@gmail.com>
2024-05-31 19:15:53 +0800

    update database

--- db/db_community.sql
+++ db/db_community.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2024 at 11:54 AM
+-- Generation Time: May 31, 2024 at 01:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
