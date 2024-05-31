97ec262628e997b9d69581d06272d97210edb7ba
yeftakun <yeftakun34@gmail.com>
2024-05-31 18:39:09 +0800

    add home page

---
+++ deniedpage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Access Denied</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            text-align: center;
+            padding-top: 100px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+</head>
+<body>
+    <h1>Access Denied</h1>
+    <p>Sorry, you don't have permission to access this page.</p>
+</body>
+</html>
