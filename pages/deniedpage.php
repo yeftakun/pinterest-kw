aa795e77ca03ea95fc23a57c3aaf704dd4610288
yeftakun <yeftakun34@gmail.com>
2024-06-01 16:26:09 +0800

    add denied page

---
+++ pages/deniedpage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Access Denied</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            text-align: center;
+            padding-top: 100px;
+        }
+        h1 {
+            color: #333;
+        }
+        p {
+            color: #666;
+        }
+    </style>
+    <link rel="icon" type="image/png" href="../assets/ico/HitoriGotou.ico">
+</head>
+<body>
+    <h1>Access Denied</h1>
+    <p>Sorry, you don't have permission to access this page.</p>
+
+    <div class="tenor-gif" width="100%">
+        <img src="../assets/gif/denied-no.gif" alt="gif no">
+    </div> 
+</body>
+</html>
