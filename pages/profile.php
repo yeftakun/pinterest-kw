ad4616b993d1f3ce89a782dc05949044952b4141
yeftakun <yeftakun34@gmail.com>
2024-06-02 16:44:41 +0800

    add beranda & profile

---
+++ pages/profile.php
@@ -0,0 +1,105 @@
+<?php
+// Include koneksi ke database
+include '../koneksi.php';
+session_set_cookie_params(3600);
+session_start();
+
+// Ambil nama pengguna dari URL
+if(isset($_GET['user_name'])) {
+    $user_name = $_GET['user_name'];
+    
+    // Query untuk mengambil informasi pengguna berdasarkan user_name
+    $getUserQuery = "SELECT * FROM users WHERE user_name = '$user_name'";
+    $getUserResult = mysqli_query($koneksi, $getUserQuery);
+
+    // Pastikan ada hasil yang ditemukan
+    if(mysqli_num_rows($getUserResult) > 0) {
+        $userData = mysqli_fetch_assoc($getUserResult);
+        
+        // Assign data pengguna ke variabel
+        $user_id = $userData['user_id'];
+        $user_name = $userData['user_name'];
+        $name = $userData['name'];
+        $user_profile_path = $userData['user_profile_path'];
+        $user_bio = $userData['user_bio'];
+        $level_id = $userData['level_id'];
+        $password = $userData['password'];
+        $status = $userData['status'];
+        $tele_chat_id = $userData['tele_chat_id'];
+    } else {
+        // Redirect ke halaman lain jika pengguna tidak ditemukan
+        header("Location: error/not_found.php");
+        exit();
+    }
+} else {
+    // Redirect ke halaman lain jika user_name tidak disediakan
+    header("Location: error/not_found.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Pengguna</title>
+</head>
+<body>
+    <?php
+    // Periksa apakah pengguna sudah login
+    if(isset($_SESSION['user_id'])) {
+        if(isset($_SESSION['user_name']) && $_SESSION['user_name'] !== $user_name)
+ {
+            ?>
+            <p><a href="#">Beranda</a> | <a href="#">Share</a></p>
+            <p>Informasi Pengguna:</p>
+            <img src="../storage/profile/<?php echo $user_profile_path; ?>" alt="<?php echo $user_profile_path; ?>" max-width="300px">
+            <ul>
+                <li>User ID : <?php echo $user_id; ?></li>
+                <li>User Name : <?php echo $user_name; ?></li>
+                <li>Nama : <?php echo $name; ?></li>
+                <li>Bio : <?php echo $user_bio; ?></li>
+                <li>Level ID : <?php echo $level_id; ?></li>
+                <li>Password : <?php echo $password; ?></li>
+                <li>Status : <?php echo $status; ?></li>
+                <li>Chat ID : <?php echo $tele_chat_id; ?></li>
+            </ul>
+            <?php
+        } else {
+            ?>
+            <p><a href="#">Beranda</a> | <a href="#">Edit Profil</a></p>
+            <p>Informasi Saya:</p>
+            <img src="../storage/profile/<?php echo $user_profile_path; ?>" alt="<?php echo $user_profile_path; ?>" max-width="300px">
+            <ul>
+                <li>User ID : <?php echo $user_id; ?></li>
+                <li>User Name : <?php echo $user_name; ?></li>
+                <li>Nama : <?php echo $name; ?></li>
+                <li>Bio : <?php echo $user_bio; ?></li>
+                <li>Level ID : <?php echo $_SESSION['level_id']; ?></li>
+                <li>Password : <?php echo $password; ?></li>
+                <li>Status : <?php echo $status; ?></li>
+                <li>Chat ID : <?php echo $tele_chat_id; ?></li>
+            </ul>
+        <?php
+        }
+
+    } else {
+        // Pengguna belum login, tampilkan link login
+        ?>
+        <p><a href="../index.php">Beranda</a> | <a href="#">Share</a> | <a href="#">Login</a></p>
+        <p>Informasi Pengguna:</p>
+        <img src="../storage/profile/<?php echo $user_profile_path; ?>" alt="<?php echo $user_profile_path; ?>" max-width="300px">
+        <ul>
+            <li>User ID : <?php echo $user_id; ?></li>
+            <li>User Name : <?php echo $user_name; ?></li>
+            <li>Nama : <?php echo $name; ?></li>
+            <li>Bio : <?php echo $user_bio; ?></li>
+            <li>Level ID : <?php echo $level_id; ?></li>
+            <li>Password : <?php echo $password; ?></li>
+            <li>Status : <?php echo $status; ?></li>
+            <li>Chat ID : <?php echo $tele_chat_id; ?></li>
+        </ul>
+        <?php
+    }
+    ?>
+</body>
+</html>
