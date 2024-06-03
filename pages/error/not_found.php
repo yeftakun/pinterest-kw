ad1ecab3846d6726669514bef71a0ef7cd56eb85
yeftakun <yeftakun34@gmail.com>
2024-06-04 01:51:31 +0800

    .

--- pages/error/not_found.php
+++ pages/error/not_found.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>404 - Not found</h1>
-    <p>Sorry, can't find the page/data</p>
+    <p>Sorry, can't find the page or data</p>
 
     <div class="tenor-gif" width="100%">
         <img src="../../assets/gif/confused.gif" alt="gif no">
