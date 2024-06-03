1c7e837f66dd4a7790e4361ba1f2e85508b8c89d
yeftakun <yeftakun34@gmail.com>
2024-06-03 08:10:55 +0800

    separate style

--- pages/error/deniedpage.php
+++ pages/error/deniedpage.php
@@ -2,24 +2,11 @@
 <html>
 <head>
     <title>Access Denied</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            text-align: center;
-            padding-top: 100px;
-        }
-        h1 {
-            color: #333;
-        }
-        p {
-            color: #666;
-        }
-    </style>
+    <link rel="stylesheet" href="../../styles/denied_page.css">
     <link rel="icon" type="image/png" href="../../assets/ico/HitoriGotou.ico">
 </head>
 <body>
-    <h1>Access Denied</h1>
+    <h1>403 - Forbiden</h1>
     <p>Sorry, you don't have permission to access this page.</p>
 
     <div class="tenor-gif" width="100%">
