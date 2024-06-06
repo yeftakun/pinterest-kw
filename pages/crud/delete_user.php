2e97294156b2820da80043543030b98df5550897
yeftakun <yeftakun34@gmail.com>
2024-06-07 06:45:30 +0800

    add notif when delete

--- pages/crud/delete_user.php
+++ pages/crud/delete_user.php
@@ -3,10 +3,13 @@ session_start();
 
 // Include file koneksi database
 include '../../koneksi.php';
+include '../../environment.php';
+
+$token = TOKEN_BOT;
 
 // Memeriksa apakah pengguna telah login
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ../../index.php?pesan=needlogin");
+    header("Location:../../index.php?pesan=needlogin");
     exit(); // Stop further execution
 }
 
@@ -21,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if ($delete_confirmation == $expected_confirmation) {
             // Query untuk menghapus akun pengguna beserta postingan dan profilnya
             $user_id = $_SESSION['user_id'];
-
+            
             // Hapus gambar postingan yang diupload user dari direktori "storage/posting/"
             $queryPostPath = "SELECT post_img_path FROM posts WHERE user_id = $user_id;";
             $resultPostPath = mysqli_query($koneksi, $queryPostPath);
@@ -29,7 +32,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $post_img_path = $rowPostPath['post_img_path'];
                 unlink("../../storage/posting/$post_img_path");
             }
+            
+            // kirim notifikasi akun terhapus ke telegram - init query
+            $queryTelegramID = "SELECT tele_chat_id FROM users WHERE user_id = $user_id;";
+            $resultTelegramID = mysqli_query($koneksi, $queryTelegramID);
+            $rowTelegramID = mysqli_fetch_assoc($resultTelegramID);
+            $teleChatID = $rowTelegramID['tele_chat_id'];
 
+            $telegramAPI = "https://api.telegram.org/bot$token/sendMessage?parse_mode=markdown&chat_id=$teleChatID&text=*Akun%20anda%20berhasil%20di%20hapus!*%0A%0ATerima%20kasih%20telah%20menggunakan%20layanan%20kami.";
+            
             // Hapus foto profil dari direktori "storage/profile/" kecuali default.png
             $queryProfilePath = "SELECT user_profile_path FROM users WHERE user_id = $user_id AND user_profile_path != 'default.png';";
             $resultProfilePath = mysqli_query($koneksi, $queryProfilePath);
@@ -37,10 +48,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $user_profile_path = $rowProfilePath['user_profile_path'];
             unlink("../../storage/profile/$user_profile_path");
 
+            file_get_contents($telegramAPI); // kirim notifikasi ke telegram
+            
             // Hapus akun dari tabel users
             $queryDeleteUser = "DELETE FROM users WHERE user_id = $user_id";
             mysqli_query($koneksi, $queryDeleteUser);
-
+            
             // menghapus semua session
             session_destroy();
 
